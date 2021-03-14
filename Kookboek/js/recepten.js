480534dcf54830af3aaa9103774e7a4433b6b5c5
Y-Aydogan <80647974+Y-Aydogan@users.noreply.github.com>
2021-03-14 21:30:00 +0100

    Version 1.0

---
+++ Kookboek/js/recepten.js
@@ -0,0 +1,39 @@
+//Recepten voor ontbijt
+
+function menemen() {
+    alert("Ingrediënten \n- Domates \n- Soğan \n- Yeşil biber \n- Yumurta \n- Tuz")
+}
+
+//Recepten voor lunch
+
+function kisir() {
+    alert("Ingrediënten \n- Bulgur \n- Kaynayan su \n- Soğan \n- Domates salçası \n- Biber salçası \n- Maydanoz \n- Karabiber \n- Toz biber \n- Pul biber")
+}
+
+function bara(){
+    alert("Ingrediënten \n- 1 paket Kendinden kabaran un \n- 1 Diş sarımsak \n- 1 Soğan \n- 1 Surinam biberi \n- 250g Ispanak \n- 400g Baramix \n- 1 Maya \n- Soğuk su")
+}
+
+function tonijnbroodje(){
+    alert("Ingrediënten \n- 1 Dolmalık biber \n- 1 Biber \n- 1 Domates \n- 1 Soğan \n- 2 Paket tuna \n- Kirmizi biber \n- Kara biber \n- Nane tuz")
+}
+
+//Recepten voor avond eten
+
+//Recepten voor lekkernijen
+
+function unKurabiye(){
+    alert("Ingrediënten \n- 1 paket Yağ \n- 1 sb Pudra şekeri \n- 1 çb Siviyağ \n- 4 sb Un  \n- 1 sb Nişasta  \n- 1 paket Vanilya \n- 1/2 paket Kabartmatozu")
+}
+
+function kavalaKurabiye(){
+    alert("Ingrediënten \n- 250g Yağ \n- 1 sb Siviyağ \n- 1 sb Pudra şekeri \n- 1 sb Hindistan cevizi  \n- 1 çb Çekilmiş ceviz  \n- 1 Yumurta \n- 1 paket Vanilya \n- 1 paket Kabartmatozu \n- 4-5 b un")
+}
+
+function kakaoluKurabiye(){
+    alert("Ingrediënten \n- 250g Yağ \n- 1 sb Pudra şekeri \n- 1 Yumurta \n- 4 yemek kaşığı Kakao  \n- 4 sb Un  \n- 1 paket Kabartmatozu")
+}
+
+function kristalKurabiye(){
+    alert("Ingrediënten \n- 1/2 paket Margarin \n- 1 çb Siviyağ \n- 1 çb Nişasta \n- 1 çb Pudra şekeri  \n- 3 yemek kaşığı kakao  \n- 1 paket Kabartmatozu \n- Un \n Şeker (bulamak için)")
+}
